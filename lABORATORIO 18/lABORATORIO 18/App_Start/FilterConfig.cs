3e078275e82fe18686b9b2d773a9556bebdd5bea
Anthony Hernandez <anthony.hernandez@utp.ac.pa>
2025-12-04 23:22:48 -0500

    Subiendo laboratorio 18

---
+++ lABORATORIO 18/lABORATORIO 18/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace lABORATORIO_18
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ lABORATORIO 18/lABORATORIO 18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lABORATORIO_18.MvcApplication" Language="C#" %>
